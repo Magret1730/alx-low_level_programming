166dc1ddadc8c21fde9636e1b2b8b8515baa1cfb
Magret <oyedeleabiodun79@gmail.com>
2023-04-20 15:40:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,14 @@
 /**
  * main - Puts out a string
 (*
- * Description: A program that prints exactly "Programming is like
+ * Description: A program that prints exactly "Programming is likie
  * building a multilingual puzzle"
  * followed by new line")?
  * Return: A string
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
