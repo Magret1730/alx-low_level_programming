31a8c6a6f6539ba4f7b8e78a0430b5113ad1bf62
Magret <oyedeleabiodun79@gmail.com>
2023-04-20 13:54:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *A program that prints exactly
+ * "Programming is like building a multilingual puzzle"
+ * followed by one line
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
