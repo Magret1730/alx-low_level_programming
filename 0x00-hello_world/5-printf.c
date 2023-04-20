eae92678832485792a7a0dd106f5ada69ae405b2
Magret <oyedeleabiodun79@gmail.com>
2023-04-20 15:59:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string
+ * @void: No parameter
+(*
+ * Description: a C program that prints exactly
+ * "with proper grammar, but the outcome is a piece of art,"
+ * , followed by a new line.
+(* stdio.h: Standard input and output
+ * Return: A string
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
