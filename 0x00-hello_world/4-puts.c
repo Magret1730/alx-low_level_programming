1fa7886f547a6da747efbc037e1e51c34cf8d65e
Magret <oyedeleabiodun79@gmail.com>
2023-04-20 14:58:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
- *A program that prints exactly
+ * A program that prints exactly
  * "Programming is like building a multilingual puzzle"
  * followed by one line
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	char name[150];
+
+	puts("\"Programming is like building a multilingual puzzle\n");
+	puts(name);
+
+	return (0);
 }
