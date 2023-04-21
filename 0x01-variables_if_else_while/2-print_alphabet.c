6a0842da8f5e1c3887f3cc08139269e0bd51c53b
Magret <oyedeleabiodun79@gmail.com>
2023-04-21 16:48:43 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - it is void
+ *
+ * Description: Prints lowercase alphabets
+ * followed by a new line
+ * stdio.h: putchar
+ * Return: 0 which means success
+ */
+
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+		putchar('\n');
+	}
+
+	return (0);
+}
