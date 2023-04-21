4f595cc8c42b1cdc531b5cb10970440a89333168
Magret <oyedeleabiodun79@gmail.com>
2023-04-21 15:24:11 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - void
+ *
+ * Description:
+ * stdlib.h: contains functions of memory allocation and process control
+ * time.h: time
+ * math.h: %
+ * stdio.h: printf
+ * Return: zero i.e success
+ */
+
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		last = (n % 10) * -1;
+	}
+	else
+	{
+	last = n % 10;
+	}
+		if (last >= 5)
+		{
+			printf("Last digit of %d is %d and is greater than 5\n", n, last);
+		}
+		else if (last == 0)
+		{
+			printf("Last digit of %d is %d and is 0\n", n, last);
+		}
+		else
+		{
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+		}
+	return (0);
+}
