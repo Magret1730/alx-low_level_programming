967ff261c64337ae1ee81b18bb483369e5111204
Magret <oyedeleabiodun79@gmail.com>
2023-04-21 17:20:53 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets
+ *
+ * Description: prints alphabets in lower case
+ * and then upper case
+ * followed by a new line
+ * stdio.h: putchar
+ * Return: 0 meaning success
+ */
+
+int main(void)
+{
+	char lower;
+	char upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			putchar(upper);
+		}
+
+	putchar(lower);
+	}
+
+	return (0);
+}
