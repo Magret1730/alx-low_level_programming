ef9ac1a12ffb9a9edf5df1cae06e53f053308416
Magret <oyedeleabiodun79@gmail.com>
2023-04-25 10:51:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - it is void
+ *
+ * Description: prints out _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar("_putchar\n");
+
+	return (0);
+}
