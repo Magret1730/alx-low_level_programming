446abaf96e76a7339cd3fae83aa89d251240a2c0
Magret <oyedeleabiodun79@gmail.com>
2023-04-26 16:01:13 -0700

    2-largest_number.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+(*
+ * Description: This program will assign a random number
+ * to the variable n each time it is executed and
+ * print whether the number stored in the
+ * variable n is positive or negative
+(* stdio.h: contains input and output function - printf
+ * stdlib.h: contains function of memory allocation
+ * time.h: contains function for working
+ * with time and date - time
+ * Return: zero which is success
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+}
