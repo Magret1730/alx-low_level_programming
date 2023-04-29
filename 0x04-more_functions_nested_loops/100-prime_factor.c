86cb86d0031f3b742869da0ffed42e96fc07853e
Magret <oyedeleabiodun79@gmail.com>
2023-04-28 17:23:40 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - prints prime factor
+ *
+ * Description: program that prints prime factor
+ * of the number 612852475143 followed by a new
+ * line
+ * Return: void
+ */
+
+int main(void)
+{
+	int largest;
+	int i;
+	long int n;
+
+	n = 612852475143;
+
+	while (n % 2 == 0)
+	{
+		n = n / 2;
+		printf("factor is 2 \n");
+		printf("n is now %ld \n", n);
+		if (largest < 2)
+		{
+			largest = 2;
+		}
+	}
+	for (i = 3; i*i <= n; i += 2)
+	{
+		while (n % i == 0)
+                {
+			n = n / i;
+			largest = i;
+		}
+
+	}
+
+	printf("%d\n", largest);
+	return 0;
+}
