e65d906eeab55a9a969b06033a2014faa3a59b92
Magret <oyedeleabiodun79@gmail.com>
2023-05-12 09:09:00 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file
+ * @argc: counts
+ * @argv[]: strings
+ *
+ * Description: program that prints its name, followed by a new line.
+ * If you rename the program, it will print the new name, without
+ * having to compile it again.ou should not remove the path before the
+ * name of the program.
+ * Return: nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+ARGC and ARGV learning with ALX
