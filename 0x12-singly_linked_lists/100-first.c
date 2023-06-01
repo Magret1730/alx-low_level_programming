b7fa979e1c1a9327845eee52309b00b4f672da21
Magret <oyedeleabiodun79@gmail.com>
2023-06-01 15:37:08 -0700

    function that add strings before main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * before_main - prints before main function is executed
+ *
+ * Description: function that prints strings before main function
+ * is executed
+ * Return: void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
