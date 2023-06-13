698aeb0fbcdc4f266a19bb502d0ea740833c2385
Magret <oyedeleabiodun79@gmail.com>
2023-06-13 14:24:22 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,5 @@
 #include "main.h"
 /**
- * handleError - Prints an error message and exits theprogramwith the errorcode
- * @error_code: The error code to exit with.
- * @message: The error message to print.
- * Return: void
- */
-void handleError(int error_code, const char *message)
-{
-	dprintf(STDERR_FILENO, "Error: %s\n", message);
-	exit(error_code);
-}
-/**
  * copyFile - function that copies the content of a file to another file
  * @file_from: source file name
  * @file_to: destination file name
@@ -25,35 +14,47 @@ int copyFile(const char *file_from, const char *file_to)
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from < 0)
-		handleError(98, "Can't read from source file");
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to < 0)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		close(fd_from);
-		handleError(99, "Can't write to destination file");
+		exit(99);
 	}
 	while ((n_read = read(fd_from, buf, sizeof(buf))) > 0)
 	{
 		n_written = write(fd_to, buf, n_read);
 		if (n_written < 0 || n_written != n_read)
 		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			close(fd_from);
 			close(fd_to);
-			handleError(99, "Can't write to destination file");
+			exit(99);
 		}
 	}
 	if (n_read < 0)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		close(fd_from);
 		close(fd_to);
-		handleError(98, "Can't read from source file");
+		exit(98);
 	}
 	close_result = close(fd_from);
 	if (close_result == -1)
-		handleError(100, "Can't close source file descriptor");
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
 	close_result = close(fd_to);
 	if (close_result == -1)
-		handleError(100, "Can't close destination file descriptor");
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 	return (0);
 }
 /**
@@ -68,7 +69,10 @@ int main(int argc, char *argv[])
 	char *file_from, *file_to;
 
 	if (argc != 3)
-		handleError(97, "Usage: cp file_from file_to");
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 	file_from = argv[1];
 	file_to = argv[2];
 	if (copyFile(file_from, file_to) == -1)
