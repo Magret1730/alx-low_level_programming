ddc91dc83281d684e4f99403b5688168a1aaae76
‘Magret’ <oyedeleabiodun@gmail.com>
2023-07-20 14:24:32 -0230

    function that deletes the node at index index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - Deletes the node at index index of a dlistint_t linked list.
+ * delete_dnodeint_at_index - Deletes the node at index
  * @head: Pointer to the head of the doubly linked list.
  * @index: Index of the node that should be deleted. Index starts at 0.
  * Return: 1 if it succeeded, -1 if it failed.
