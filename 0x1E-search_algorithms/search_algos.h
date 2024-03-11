c88e04774a13396ba5cfe40561388c64dd8d1ece
Magret <belloabiodun17@gmail.com>
2024-03-11 06:45:42 -0700

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H_
+#define SEARCH_ALGOS_H_
+
+/* Standard libraries */
+#include <stddef.h>
+#include <stdio.h>
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
